ec51c9c3ed4dc5e9b5ed32cbc60b234c39efbb9b
shamilkotta <shamilkotta99@gmail.com>
2022-11-17 20:37:54 +0530

    'styling

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,10 @@ module.exports = {
     commonjs: true,
     es2021: true,
   },
-  extends: ['airbnb-base', 'prettier'],
-  overrides: [
-  ],
+  extends: ["airbnb-base", "prettier"],
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaVersion: "latest",
   },
-  rules: {
-  },
+  rules: {},
 };
