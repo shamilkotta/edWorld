2cf92c1b901a1c2c11147a6a085f05119fbd6648
shamilkotta <shamilkotta99@gmail.com>
2022-11-17 21:49:53 +0530

    force styling

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const http = require("http");
 const app = require("./app");
 
-const a = 23
+
 
 const port = process.env.PORT || 5000;
-app.set("port", port);
+app.set('port', port);
 
 const server = http.createServer(app);
 server.listen(port, () => {
