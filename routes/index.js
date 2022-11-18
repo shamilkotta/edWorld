522668fdeb723c2837ec0e31b533fa177a233255
shamilkotta <shamilkotta99@gmail.com>
2022-11-18 10:04:52 +0530

    login route setup

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,23 @@ router.get("/login", (req, res) => {
 });
 
 router.get("/office-login", (req, res) => {
-  res.render("office-login");
+  const error = req.session.officLoginError;
+  res.render("office-login", { error });
+  req.session.officLoginError = "";
+});
+
+router.post("/office-login", (req, res) => {
+  const { username, password } = req.body;
+  const name = process.env.OFFICE || "admin";
+  const pass = process.env.OFFICE_PASS || "admin123";
+  if (username === name && password === pass) {
+    req.session.loggedIn = true;
+    req.session.user = { role: "office" };
+    res.redirect("/office");
+  } else {
+    req.session.officLoginError = "Invalid user name or password";
+    res.redirect("/office-login");
+  }
 });
 
 router.get("/forgot-password", (req, res) => {
