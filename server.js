677e87ff101aceaf2dbc7610fc499e468755e785
shamilkotta <shamilkotta99@gmail.com>
2022-11-18 05:43:47 +0530

    workflow setup

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const http = require("http");
 const app = require("./app");
 
-
-
 const port = process.env.PORT || 5000;
-app.set('port', port);
+app.set("port", port);
 
 const server = http.createServer(app);
 server.listen(port, () => {
