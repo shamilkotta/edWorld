caf82b3247b9998db3babab659346bd8bc3ac040
shamilkotta <shamilkotta99@gmail.com>
2022-11-18 05:44:44 +0530

    basic route setup and error handler

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require("express");
+const errorHandler = require("./middlewares/errorHandler");
+const ErrorResponse = require("./utils/ErrorResponse");
 require("dotenv").config();
 
 const app = express();
+
+// body parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// route setup
+app.use("/office");
+app.use("/teacher");
+app.use("/student");
+app.use("/");
+
+// 404 request
+app.use((req, res, next) => {
+  next(new ErrorResponse(404));
+});
+
+// error handling
+app.use(errorHandler);
 
 module.exports = app;
