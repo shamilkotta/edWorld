9ac74513ff3bd2e2a5affa530c04694b654846e6
shamilkotta <shamilkotta99@gmail.com>
2022-11-19 12:06:35 +0530

    unique code generator added

---
+++ helpers/office.js
@@ -0,0 +1,35 @@
+const Batch = require("../models/batch");
+const Teacher = require("../models/teacher");
+
+const Student = "";
+
+module.exports = {
+  generateUniqueCode: (data) =>
+    new Promise((resolve, reject) => {
+      let model;
+      let pre;
+      let num;
+      if (data === "batch") {
+        model = Batch;
+        pre = "BH";
+      } else if (data === "teacher") {
+        model = Teacher;
+        pre = "TR";
+      } else {
+        model = Student;
+        pre = "ST";
+      }
+
+      model
+        .countDocuments({})
+        .then((count) => {
+          if (count < 10) num = `00${count + 1}`;
+          else if (count > 9 && count < 100) num = `0${count + 1}`;
+          const code = `${pre}${num}`;
+          resolve(code);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    }),
+};
