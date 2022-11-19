b617ad2c8def73b179c8575fd55a0b12da44ccbb
shamilkotta <shamilkotta99@gmail.com>
2022-11-19 10:51:56 +0530

    teacher model created

---
+++ models/teacher.js
@@ -0,0 +1,94 @@
+const mongoose = require("mongoose");
+
+const teacherSchema = new mongoose.Schema({
+  registerId: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    uppercase: true,
+    immutable: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  birth_date: {
+    type: Date,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+    default: "Not specified",
+    validate: {
+      validator: (arg) =>
+        ["Male", "Femail", "Other", "Not specified"].includes(arg),
+    },
+  },
+  salary: {
+    type: Number,
+    required: true,
+  },
+  address: {
+    house_name: {
+      type: String,
+      trim: true,
+    },
+    place: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    post: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    pin: {
+      type: Number,
+      required: true,
+    },
+    district: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    state: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  qualification: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  experience: {
+    type: Number,
+    required: true,
+    default: 0,
+    validate: {
+      validator: (arg) => arg >= 0,
+    },
+  },
+  profile: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
+
+module.exports = mongoose.model("Teacher", teacherSchema);
