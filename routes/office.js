ff1a28f0387434c0a461c6c4bf2bc7cce78fcdb4
shamilkotta <shamilkotta99@gmail.com>
2022-11-19 06:41:20 +0530

    add batch routes created

--- routes/office.js
+++ routes/office.js
@@ -1,5 +1,8 @@
 const express = require("express");
 
+const batchValidations = require("../middlewares/validations/batchValidations");
+const Batch = require("../models/batch");
+
 const router = express.Router();
 
 // router level layout setting
@@ -10,6 +13,34 @@ router.all("/*", (req, res, next) => {
 
 router.get("/", (req, res) => {
   res.render("office/index");
+});
+
+router.get("/batches/add-btach", batchValidations, (req, res) => {
+  res.render("", { error: req.session.addBatchError });
+  req.session.addBatchError = "";
+});
+
+router.post("/batches/add-btach", batchValidations, (req, res) => {
+  if (req.validationErr) {
+    req.session.addBatchError = req.validationErr;
+    res.redirect("/batches/add-batch");
+  } else {
+    const data = req.validData;
+    const batch = new Batch(data);
+    batch
+      .save()
+      .then(() => {
+        req.session.addBatchSuccess = "New batch created successfully";
+        res.redirect("/batches/add-batch");
+      })
+      .catch((err) => {
+        if (err.code === 11000)
+          req.session.addBatchError = "Duplicate batch code";
+        else req.session.addBatchError = "Something went wrong";
+        res.redirect("/batches/add-batch");
+      });
+  }
+  res.end();
 });
 
 module.exports = router;
