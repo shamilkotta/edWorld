4471c669f3e99586cc398eab9a3e8516bfbc7f1a
shamilkotta <shamilkotta99@gmail.com>
2022-11-19 06:39:49 +0530

    batch model created

---
+++ models/batch.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const batchSchema = new mongoose.Schema({
+  code: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    uppercase: true,
+  },
+  start_date: {
+    type: Date,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+    immutable: true,
+  },
+  fee: {
+    type: Number,
+    required: true,
+    immutable: true,
+  },
+  seat_num: {
+    type: Number,
+    required: true,
+  },
+  batch_head: {
+    type: String,
+    required: true,
+  },
+  fee_type: {
+    type: [String],
+    required: true,
+    default: ["One time"],
+    validate: {
+      validator: (arg) =>
+        arg.length !== 0 &&
+        arg.every((ele) => ["One time", "Installment"].includes(ele)),
+    },
+  },
+});
+
+module.exports = mongoose.model("Batch", batchSchema);
