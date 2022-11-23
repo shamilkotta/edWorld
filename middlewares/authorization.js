bb4c6ea43268f3afc91d3e8f0af85e772cd67ec0
shamilkotta <shamilkotta99@gmail.com>
2022-11-23 23:10:56 +0530

    authorization middleware for routes

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -7,4 +7,20 @@ module.exports = {
       next(new ErrorResponse(404));
     else res.redirect("/office-login");
   },
+
+  teacherAuthorization: (req, res, next) => {
+    if (req.session.loggedIn && req.session.user.role === "teacher") {
+      if (req.session.user.isPasswordStrong) next();
+      else res.redirect("/update-password");
+    } else if (req.session.loggedIn) next(new ErrorResponse(404));
+    else res.redirect("/login");
+  },
+
+  studentAuthorization: (req, res, next) => {
+    if (req.session.loggedIn && req.session.user.role === "student") {
+      if (req.session.user.isPasswordStrong) next();
+      else res.redirect("/update-password");
+    } else if (req.session.loggedIn) next(new ErrorResponse(404));
+    else res.redirect("/login");
+  },
 };
