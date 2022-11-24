192c4d479d2691220c520eb02629dfcfde1e082c
shamilkotta <shamilkotta99@gmail.com>
2022-11-24 11:15:13 +0530

    404 page created

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -2,7 +2,8 @@
 /* eslint-disable prefer-const */
 module.exports = (err, req, res, next) => {
   let { status = 500, message = "Something went wrong" } = err;
-  if (status === 404) message = "Not found";
-  if (status === 401) message = "Unauthorized";
+  if (status === 404) {
+    res.redirect("/404");
+  } else if (status === 401) message = "Unauthorized";
   res.status(status).json({ success: false, status, message });
 };
