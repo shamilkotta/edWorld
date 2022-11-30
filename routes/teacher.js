84805cbe6ce5047b3d3747af1e445647b0abf965
shamilkotta <shamilkotta99@gmail.com>
2022-11-30 15:59:19 +0530

    validation middleware and monthly data route

--- routes/teacher.js
+++ routes/teacher.js
@@ -6,10 +6,15 @@ const {
   changePassword,
   getStudent,
   putTotalWrokingDays,
+  putMonthlyData,
 } = require("../controllers/teacher");
 const {
   editTeacherValidationUser,
 } = require("../middlewares/validations/office/teacherValidations");
+const {
+  totalWorkingDaysValidation,
+  monthlyDataValidation,
+} = require("../middlewares/validations/teacher/validations");
 
 const router = express.Router();
 
@@ -29,6 +34,12 @@ router.put("/change-password", changePassword);
 
 router.get("/classroom/:registerId", getStudent);
 
-router.put("/add-total-working-days", putTotalWrokingDays);
+router.put(
+  "/add-total-working-days",
+  totalWorkingDaysValidation,
+  putTotalWrokingDays
+);
+
+router.put("/add-monthly-data", monthlyDataValidation, putMonthlyData);
 
 module.exports = router;
