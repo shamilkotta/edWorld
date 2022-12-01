784472059162e25a4864056b486ea26e908aa8ef
shamilkotta <shamilkotta99@gmail.com>
2022-12-01 20:48:53 +0530

    students fee payment status fetch api

--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,66 @@
 const { create } = require("express-handlebars");
+const Student = require("../models/student");
 
 module.exports = {
   compileHTMLEmailTemplate: (HTMLTemplatePath, replacements = {}) =>
     new Promise((resolve) => {
       const compiledHTML = create().render(HTMLTemplatePath, replacements);
       resolve(compiledHTML);
+    }),
+
+  getFeeData: (registerId) =>
+    new Promise((resolve, reject) => {
+      Student.aggregate([
+        {
+          $match: {
+            registerId,
+          },
+        },
+        {
+          $lookup: {
+            from: "batches",
+            localField: "batch",
+            foreignField: "code",
+            pipeline: [
+              {
+                $project: {
+                  _id: 0,
+                  fee: 1,
+                  fee_type: 1,
+                },
+              },
+            ],
+            as: "batchData",
+          },
+        },
+        {
+          $unwind: "$batchData",
+        },
+        {
+          $project: {
+            _id: 0,
+            registerId: 1,
+            batch: 1,
+            payment_done: 1,
+            installment: 1,
+            amount: "$batchData.fee",
+            feeOptions: {
+              $cond: {
+                if: {
+                  $in: ["Installment", "$batchData.fee_type"],
+                },
+                then: 1,
+                else: 0,
+              },
+            },
+          },
+        },
+      ])
+        .then((data) => {
+          resolve(data[0]);
+        })
+        .catch((err) => {
+          reject(err);
+        });
     }),
 };
