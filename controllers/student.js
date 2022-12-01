d60aa193d39c6739f3534710d44454be81b6b7e5
shamilkotta <shamilkotta99@gmail.com>
2022-12-01 20:50:02 +0530

    invoice data generator

--- controllers/student.js
+++ controllers/student.js
@@ -1,3 +1,5 @@
+/* eslint-disable camelcase */
+const { getFeeData } = require("../helpers");
 const { getAllStudentsData } = require("../helpers/office");
 
 const superfix = (index, inc = false) => {
@@ -11,6 +13,148 @@ const superfix = (index, inc = false) => {
   if (index === 2 || index === "2") return "2<sup>nd</sup>";
   if (index === 3 || index === "3") return "3<sup>rd</sup>";
   return `${index}<sup>th</sup>`;
+};
+
+const getInvoiceData = async (registerId, option) => {
+  try {
+    const feeData = await getFeeData(registerId);
+    if (!feeData)
+      return {
+        success: false,
+        message: "Can't fetch your invoice, please try again later",
+      };
+
+    // else
+    const { feeOptions, amount, payment_done, installment } = feeData;
+    const tax = 0.0;
+    const taxAmount = 0.0;
+    const first = amount / 3.0;
+    const firstAmount = Math.round(first / 100) * 100;
+    const second = (amount - firstAmount) / 2.0;
+    const secondAmount = Math.round(second / 100) * 100;
+    const thirdAmount = amount - (secondAmount + firstAmount);
+
+    // if user selected 'installment' and has 'installment' option enabled
+    if (option === "1" && feeData.feeOptions) {
+      // payment already done
+      if (payment_done)
+        return {
+          success: false,
+          statusCode: 200,
+          errorType: "completed alredy",
+          message: "You have completed your fee payment already, thank you",
+        };
+
+      // pending payment
+      let currentAmount;
+      let currentBalance;
+
+      if (installment === 0) {
+        currentAmount = firstAmount;
+        currentBalance = amount - firstAmount;
+      } else if (installment === 1) {
+        currentAmount = secondAmount;
+        currentBalance = amount - (firstAmount + secondAmount);
+      } else if (installment === 2) {
+        currentAmount = thirdAmount;
+        currentBalance = 0.0;
+      } else {
+        currentAmount = 0.0;
+        currentBalance = 0.0;
+      }
+
+      const data = {
+        feeOptions,
+        amount: currentAmount,
+        tax,
+        taxAmount,
+        total: currentAmount + taxAmount,
+        balance: currentBalance,
+      };
+
+      return {
+        success: true,
+        data,
+      };
+
+      // if user selected 'onetime' and has 'installment' option enabled
+    }
+    if (option === "0" && feeData.feeOptions) {
+      // payment alredy done
+      if (payment_done)
+        return {
+          success: false,
+          statusCode: 200,
+          errorType: "completed alredy",
+          message: "You have completed your fee payment already, thank you",
+        };
+
+      // payment pending
+      let currentAmount;
+
+      if (installment === 3) currentAmount = 0.0;
+      else if (installment === 2)
+        currentAmount = amount - (secondAmount + firstAmount);
+      else if (installment === 1) currentAmount = amount - firstAmount;
+      else currentAmount = amount;
+
+      const data = {
+        feeOptions,
+        amount: currentAmount,
+        tax,
+        taxAmount,
+        total: currentAmount + taxAmount,
+        balance: 0.0,
+      };
+
+      return {
+        success: true,
+        data,
+      };
+
+      // if user selected 'onetime' and has 'no installment' option
+    }
+    if (option === "0" && !feeData.feeOptions) {
+      // payment alredy done
+      if (payment_done)
+        return {
+          success: false,
+          statusCode: 200,
+          errorType: "completed alredy",
+          message: "You have completed your fee payment already, thank you",
+        };
+
+      // payment pending
+      const data = {
+        feeOptions,
+        amount,
+        tax,
+        taxAmount,
+        total: amount + taxAmount,
+        balance: 0.0,
+      };
+
+      return {
+        success: true,
+        data,
+      };
+
+      // invalid option combo
+    }
+    return {
+      success: false,
+      statusCode: 404,
+      errorType: "can't fetch",
+      message: "Can't fetch your invoice, please try again later",
+    };
+  } catch (error) {
+    return {
+      success: false,
+      statusCode: 404,
+      errorType: "can't fetch",
+      message: "Can't fetch your invoice, please try again later",
+    };
+  }
 };
 
 module.exports = {
